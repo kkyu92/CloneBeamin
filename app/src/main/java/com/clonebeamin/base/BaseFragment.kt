899b687f3789773000e619a5ef94935b6bd2eff2
kyusikkyu <kyusikkyu@gmail.com>
2021-09-14 22:42:30 +0900

    [Refactor] BaseFragment / BaseViewModel

--- app/src/main/java/com/clonebeamin/base/BaseFragment.kt
+++ app/src/main/java/com/clonebeamin/base/BaseFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< HEAD
 import android.widget.Toast
-=======
->>>>>>> origin/master
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-<<<<<<< HEAD
 
 abstract class BaseFragment<B : ViewDataBinding>(
     @LayoutRes val layoutId: Int
@@ -44,33 +40,4 @@ abstract class BaseFragment<B : ViewDataBinding>(
 
     protected fun showToast(msg: String) =
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-=======
-import io.reactivex.disposables.CompositeDisposable
-
-abstract class BaseFragment<B : ViewDataBinding>(
-    @LayoutRes private val layoutRes: Int
-) : Fragment() {
-
-    private val compositeDisposable = CompositeDisposable()
-    private var _binding: B? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        compositeDisposable.clear()
-        super.onDestroyView()
-    }
-    override fun onDestroy() {
-        compositeDisposable.dispose()
-        super.onDestroy()
-    }
->>>>>>> origin/master
 }
